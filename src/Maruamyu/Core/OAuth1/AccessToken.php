dcf55c5d6cc8a40ee4a32b992ae5105944c432d0
にゃー (mirai-iro) <mirai-iro@maruamyu.net>
2020-02-03 21:22:03 +0900

    OAuth1 AccessToken::__toString()

--- src/Maruamyu/Core/OAuth1/AccessToken.php
+++ src/Maruamyu/Core/OAuth1/AccessToken.php
@@ -2,6 +2,8 @@
 
 namespace Maruamyu\Core\OAuth1;
 
+use Maruamyu\Core\Http\Message\QueryString;
+
 /**
  * oauth_token and oauth_token_secret
  */
@@ -25,6 +27,39 @@ class AccessToken
     {
         $this->token = strval($token);
         $this->tokenSecret = strval($tokenSecret);
+    }
+
+    /**
+     * @param string $queryString
+     * @return static|null
+     */
+    public static function fromQueryString($queryString)
+    {
+        $data = QueryString::parse($queryString);
+        if (isset($data, $data['oauth_token'], $data['oauth_token_secret'])) {
+            return new static($data['oauth_token'][0], $data['oauth_token_secret'][0]);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @return string QUERY_STRING
+     */
+    public function __toString()
+    {
+        return QueryString::build($this->toArray());
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return [
+            'oauth_token' => $this->getToken(),
+            'oauth_token_secret' => $this->getTokenSecret(),
+        ];
     }
 
     /**
--- tests/Maruamyu/Core/OAuth1/AccessTokenTest.php
+++ tests/Maruamyu/Core/OAuth1/AccessTokenTest.php
@@ -15,4 +15,21 @@ class AccessTokenTest extends \PHPUnit\Framework\TestCase
         $accessToken = new AccessToken('sakuraba', 'kaoru');
         $this->assertEquals('kaoru', $accessToken->getTokenSecret());
     }
+
+    public function test_fromQueryString()
+    {
+        $accessToken = AccessToken::fromQueryString('oauth_token=kashiwagi&oauth_token_secret=tsubasa');
+        $this->assertNotNull($accessToken);
+        $this->assertEquals('kashiwagi', $accessToken->getToken());
+        $this->assertEquals('tsubasa', $accessToken->getTokenSecret());
+
+        $invalidAccessToken = AccessToken::fromQueryString('oauth_token=yamamura');
+        $this->assertNull($invalidAccessToken);
+    }
+
+    public function test_toString()
+    {
+        $accessToken = new AccessToken('yamamura', 'ken');
+        $this->assertEquals('oauth_token=yamamura&oauth_token_secret=ken', strval($accessToken));
+    }
 }
