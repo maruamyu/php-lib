143cd96d3c7615b954ec5d30f18d1b00790b6d6f
にゃー (mirai-iro) <mirai-iro@maruamyu.net>
2020-03-14 01:20:47 +0900

    add ULID

--- tests/Maruamyu/Core/UuidTest.php
+++ tests/Maruamyu/Core/UuidTest.php
@@ -4,33 +4,27 @@ namespace Maruamyu\Core;
 
 class UuidTest extends \PHPUnit\Framework\TestCase
 {
+    const EXPECTS_REGEXP = '/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/u';
+
     public function test_generate()
     {
-        $expectsRegExp = '/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/u';
-        $this->assertRegExp($expectsRegExp, Uuid::generate());
-        $this->assertRegExp($expectsRegExp, Uuid::generate());
-        $this->assertRegExp($expectsRegExp, Uuid::generate());
-        $this->assertRegExp($expectsRegExp, Uuid::generate());
-        $this->assertRegExp($expectsRegExp, Uuid::generate());
-        $this->assertRegExp($expectsRegExp, Uuid::generate());
-        $this->assertRegExp($expectsRegExp, Uuid::generate());
-        $this->assertRegExp($expectsRegExp, Uuid::generate());
-        $this->assertRegExp($expectsRegExp, Uuid::generate());
-        $this->assertRegExp($expectsRegExp, Uuid::generate());
+        $generated = [];
+        for ($i = 0; $i < 100; $i++) {
+            $uuid = Uuid::generate();
+            $this->assertRegExp(self::EXPECTS_REGEXP, $uuid);
+            $this->assertArrayNotHasKey($uuid, $generated);
+            $generated[$uuid] = $i;
+        }
     }
 
     public function test_generateV4()
     {
-        $expectsRegExp = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/u';
-        $this->assertRegExp($expectsRegExp, Uuid::generateV4());
-        $this->assertRegExp($expectsRegExp, Uuid::generateV4());
-        $this->assertRegExp($expectsRegExp, Uuid::generateV4());
-        $this->assertRegExp($expectsRegExp, Uuid::generateV4());
-        $this->assertRegExp($expectsRegExp, Uuid::generateV4());
-        $this->assertRegExp($expectsRegExp, Uuid::generateV4());
-        $this->assertRegExp($expectsRegExp, Uuid::generateV4());
-        $this->assertRegExp($expectsRegExp, Uuid::generateV4());
-        $this->assertRegExp($expectsRegExp, Uuid::generateV4());
-        $this->assertRegExp($expectsRegExp, Uuid::generateV4());
+        $generated = [];
+        for ($i = 0; $i < 100; $i++) {
+            $uuid = Uuid::generateV4();
+            $this->assertRegExp(self::EXPECTS_REGEXP, $uuid);
+            $this->assertArrayNotHasKey($uuid, $generated);
+            $generated[$uuid] = $i;
+        }
     }
 }
